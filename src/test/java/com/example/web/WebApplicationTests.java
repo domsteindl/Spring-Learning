c9834103cc7487125b9bfe71fe6902d79a75902c
Dominik Steindl <dominik.steindl@gmx.de>
2025-02-25 20:09:00 +0100

    Tests für die Records Greeting und Farewell hinzugefügt

--- src/test/java/com/example/web/WebApplicationTests.java
+++ src/test/java/com/example/web/WebApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() {
+        ResponseEntity<Greeting> response = restTemplate.getForEntity("/greeting", Greeting.class);
+        Assertions.assertNotNull(response.getBody());
+        assertThat(response.getBody().content()).isEqualTo("Hello, World!");
+    }
+
+    @Test
+    void farewellShouldReturnMorningMessage() {
+        ResponseEntity<Farewell> response = restTemplate.getForEntity("/farewell?name=Dominik&timeOfDay=morning", Farewell.class);
+        Assertions.assertNotNull(response.getBody());
+        assertThat(response.getBody().message()).isEqualTo("Have a great morning, Dominik!");
+    }
 
 }
